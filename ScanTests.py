209c5bc848d40e00aeb00e31bb8c5490e7a1857d
Djcookie78 <bxl@oxo.dk>
2022-08-25 05:16:52 +0200

    scan sub

---
+++ ScanTests.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import pandas as pd
+import time
+
+start_time = time.time()
+
+# set root path
+path_root = "C:\\1\\"
+
+# create class for scanning files
+class ScanFiles:
+    def __init__(self, path_root):
+        self.path_root = path_root
+        
+    # define function for scanning files in all subdirectories
+    def scan_sub(self):
+        for root, dirs, files in os.walk(self.path_root):
+            for file in files:
+                # if file ends with .xls or .xlsx and matches E160 in filename
+                if file.endswith(".xls") or file.endswith(".xlsx") and "E160" in file:
+                    # print path to file with slashes
+                    print(os.path.join(root, file))
+                else:
+                    pass
+            
+
+ScanAll = ScanFiles(path_root)
+ScanAll.scan_sub()
